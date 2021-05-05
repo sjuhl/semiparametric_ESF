ca07c2b08d94a451719a65902e657c8091375baa
sjuhl <sebastian.juhl@gess.uni-mannheim.de>
2021-05-05 10:38:40 +0200

    simulation script & W matrices

--- MonteCarlo.R
+++ MonteCarlo.R
@@ -1,1 +1,97 @@
+##########################
+# Monte Carlo Simulations
+##########################
+
+# clean environment and set working directory
+rm(list=ls())
+
+# load spatial packages
+library(spdep) # for W
+library(spfilteR) # for filtering
+
+# parallel computing
+library(foreach)
+library(doParallel)
+library(doRNG)
+
+# get simulation function
+source("./simfunc.R")
+
+# load matrices and standardize them (spectral normalization)
 load("./W.RData")
+
+z.W_states <- W_states/as.numeric(eigen(W_states)$values[1])
+z.W_cities <- W_cities/as.numeric(eigen(W_cities)$values[1])
+z.W_cd116 <- W_cd116/as.numeric(eigen(W_cd116)$values[1])
+
+# check
+all.equal(target=1, current=as.numeric(eigen(z.W_states)$values[1]))
+all.equal(target=1, current=as.numeric(eigen(z.W_cities)$values[1]))
+all.equal(target=1, current=as.numeric(eigen(z.W_cd116)$values[1]))
+
+
+
+###################
+# MONTE CARLO SIMULATION
+
+# parameters
+b <- 1
+sd.e <- 1 # standard deviation of disturbances
+
+# calculate spatial multipliers
+p <- c(0,.25,.5,.75)
+W <- list(z.W_states,z.W_cities,z.W_cd116)
+W_id <- c(1,2,3)
+
+grid <- matrix(as.matrix(cbind(expand.grid(p,W_id),seq_len(length(p)*length(W)))),ncol=3
+               ,dimnames=list(NULL, c("p", "W_id","multi_id")))
+multipliers <- apply(grid,1,function(x) solve(diag(1,nrow(W[[x[2]]]))-x[1]*W[[x[2]]]))
+
+# cross-sections
+n <- sapply(W, nrow)
+
+ninput <- nrow(grid)
+nsim <- 1000
+
+# specify function inputs
+input <- data.frame(do.call(rbind,replicate(nsim,grid,simplify=F)))
+input <- input[order(input$W_id,input$p,input$multi_id),]
+rownames(input) <- 1:nrow(input)
+
+# check
+nrow(input)==ninput*nsim
+
+
+# test
+sim_func(spmultiplier=multipliers[[input$multi_id[1]]]
+         ,beta=b,sd.e=sd.e,W=W[[input$W_id[1]]],SEM=F,ideal.setsize=F)
+
+
+# parallel computing
+(ncores <- detectCores())
+nworkers <- ncores - 1
+cl <- makeCluster(nworkers)
+registerDoParallel(cl)
+registerDoRNG(12345)
+
+# start timer
+start.time <- Sys.time()
+# simulate
+sim_out <- foreach(i=1:nrow(input), #.combine=rbind,
+                   .packages=c("spdep")
+                   ) %dopar% {
+                     sim_func(spmultiplier=multipliers[[input$multi_id[i]]]
+                              ,beta=b,sd.e=sd.e,W=W[[input$W_id[i]]],SEM=F,ideal.setsize=F)
+                     }
+stopCluster(cl)
+
+(time.taken <- Sys.time() - start.time)
+session_info <- sessionInfo()
+
+# create empty folder (if not existing already)
+ifelse(!dir.exists(file.path("./SimOut"))
+       ,dir.create(file.path("./SimOut")), FALSE)
+
+# save output
+save(input,sim_out,nsim,time.taken,session_info,file="./SimOut/MC_Output.RData")
+
